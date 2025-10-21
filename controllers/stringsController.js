e1a4b5bcc1fd976f22646a6f7196f50855d84465
AbdullahiAA <osuolaleabdullahi@gmail.com>
2025-10-21 17:48:45 +0100

    wip

--- controllers/stringsController.js
+++ controllers/stringsController.js
@@ -1,4 +1,3 @@
-const crypto = require("crypto");
 const fs = require("fs");
 const path = require("path");
 const { isPalindrome, createSha256Hash } = require("../utils");
@@ -71,51 +70,6 @@ class StringsController {
       fs.writeFileSync(dbPath, dbContent, "utf8");
 
       return res.status(200).json(data);
-    } catch (error) {
-      console.error("Error:", error?.message);
-
-      return res.status(500).json({
-        status: "error",
-        message: "Internal server error",
-        error: error?.message,
-      });
-    }
-  }
-
-  static async getSpecificString(req, res) {
-    try {
-      let value = req.params?.value;
-
-      if (!value) {
-        return res.status(400).json({
-          status: "error",
-          message: "Value is required",
-        });
-      }
-
-      if (typeof value !== "string") {
-        return res.status(422).json({
-          status: "error",
-          message: "Value must be a string",
-        });
-      }
-
-      // Trim the value
-      value = value.trim();
-      const sha256_hash = createSha256Hash(value);
-
-      const existingEntry = (stringsDb || []).find(
-        (entry) => entry.id === sha256_hash
-      );
-
-      if (!existingEntry) {
-        return res.status(404).json({
-          status: "error",
-          message: "String not found",
-        });
-      }
-
-      return res.status(200).json(existingEntry);
     } catch (error) {
       console.error("Error:", error?.message);
 
@@ -264,6 +218,51 @@ class StringsController {
       };
 
       return res.status(200).json(response);
+    } catch (error) {
+      console.error("Error:", error?.message);
+
+      return res.status(500).json({
+        status: "error",
+        message: "Internal server error",
+        error: error?.message,
+      });
+    }
+  }
+
+  static async getSpecificString(req, res) {
+    try {
+      let value = req.params?.value;
+
+      if (!value) {
+        return res.status(400).json({
+          status: "error",
+          message: "Value is required",
+        });
+      }
+
+      if (typeof value !== "string") {
+        return res.status(422).json({
+          status: "error",
+          message: "Value must be a string",
+        });
+      }
+
+      // Trim the value
+      value = value.trim();
+      const sha256_hash = createSha256Hash(value);
+
+      const existingEntry = (stringsDb || []).find(
+        (entry) => entry.id === sha256_hash
+      );
+
+      if (!existingEntry) {
+        return res.status(404).json({
+          status: "error",
+          message: "String not found",
+        });
+      }
+
+      return res.status(200).json(existingEntry);
     } catch (error) {
       console.error("Error:", error?.message);
 
